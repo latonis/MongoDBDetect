cc695508d622bbf3bf7fcec35fa512d00524b09b
Jacob Latonis <jlatonis@me.com>
2023-09-20 22:42:38 -0500

    rule file in python example

---
+++ rules/shadow.py
@@ -0,0 +1,10 @@
+def rule(event: dict) -> bool:
+    if "/etc/shadow" in event.get("command_line"):
+        return True
+    return False
+
+"""
+detection_logic:
+  command_line: 
+    - /etc/shadow
+"""
